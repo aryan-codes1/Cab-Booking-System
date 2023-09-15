4478ab87983c2695c312d83c3b7c16e54b27ea74
sonalikeshri04 <skkeshrirng@gmail.com>
2023-09-15 20:40:26 +0530

    Updated ConnectionClass

--- src/cab/management/ConnectionClass.java
+++ src/cab/management/ConnectionClass.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cab.management;
-
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author dkkes
  */
 public class ConnectionClass {
-    
+    Connection con;
+    Statement stm;
+    ConnectionClass(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/cab_management";
+            String username= "root";
+            String password="";
+            con=DriverManager.getConnection(url,username,password);
+            if(con.isClosed()){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnectionClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public static void main(String args[]){
+        new ConnectionClass();
+    }
 }
+
